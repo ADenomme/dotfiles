a01f2e41607c3d31fdd6c0b9f5954818562e636f
Alex Denomme <alex.a.denomme@gmail.com>
2025-10-14 20:42:49 -0400

    Added keymap to clear currently highlighted items

--- nvim/lua/alex/keymaps.lua
+++ nvim/lua/alex/keymaps.lua
@@ -2,7 +2,6 @@
 vim.g.mapleader = " "
 
 -- Yank to system clipboard with ys
--- Yank to system clipboard with 'ys'
 vim.keymap.set("n", "ys", '"+yy', { noremap = true, silent = true, desc = "Yank line to system clipboard" })
 vim.keymap.set("v", "ys", '"+y', { noremap = true, silent = true, desc = "Yank selection to system clipboard" })
 
@@ -16,6 +15,10 @@ vim.keymap.set("n", "<leader>cm", function()
   vim.cmd("%y+")
   vim.notify("Copied :messages to clipboard", vim.log.levels.INFO)
 end, { desc = "Copy :messages to clipboard" })
+
+-- Clear highlighting in editor :roh
+-- e.g., gd
+vim.keymap.set("n", "<leader>hc", "<cmd>noh", { noremap = true, silent = true, desc = "Clear current highlighting" })
 
 -- Telescope keymaps
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
