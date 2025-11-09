c5ab2cde0c70a19c1613f8bb863b2d40082411f6
Alex Denomme <alex.a.denomme@gmail.com>
2025-11-09 08:07:26 -0500

    Set autocomplete to only start after typing 4 characters and limited the number of suggestsions on screen so its not distracting

--- nvim/lua/alex/autocmp.lua
+++ nvim/lua/alex/autocmp.lua
@@ -30,10 +30,17 @@ cmp.setup({
     end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
+    { name = "nvim_lsp", keyword_length = 4 },
+    { name = "luasnip",  keyword_length = 4 },
   }, {
-    { name = "buffer" },
-    { name = "path" },
+    { name = "buffer", keyword_length = 4 },
+    { name = "path",   keyword_length = 4 },
   }),
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+    keyword_length = 4,
+  },
+  performance = {
+    max_view_entries = 10, -- limits visible items in the menu
+  },
 })
