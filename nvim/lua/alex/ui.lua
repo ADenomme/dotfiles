72341280d1f0b994f65604c64a814fce544457ce
Alex Denomme <alex.a.denomme@gmail.com>
2025-11-09 08:06:13 -0500

    Created function for setting nvim color scheme with a string arg

--- nvim/lua/alex/ui.lua
+++ nvim/lua/alex/ui.lua
@@ -4,7 +4,7 @@ vim.o.relativenumber = true
 vim.o.tabstop        = 2
 vim.o.shiftwidth     = 2
 vim.o.expandtab      = true
-vim.o.textwidth       = 100
+vim.o.textwidth      = 100
 
 -- Code folding
 vim.o.foldmethod     = "expr"
@@ -31,19 +31,22 @@ function _G.custom_fold_text()
   return string.format("  %s  ⸱ %d lines ", line, lines_count)
 end
 
--- Color scheme
-vim.o.background = 'dark' -- dark / light
-require('vscode').setup({
+-- Function to set color scheme based on name
+function SetColorScheme(name)
+  vim.o.background = 'dark' -- default background
 
-  -- transparent mode
-  transparent = true,
+  if name == 'tokyo-night' then
+    vim.cmd('colorscheme tokyonight')
+  elseif name == 'vscode' then
+    require('vscode').setup({
+      transparent = true,
+      italic_comments = true,
+      terminal_colors = true,
+    })
+    vim.cmd('colorscheme vscode')
+  else
+    vim.notify('Color scheme "' .. name .. '" not recognized', vim.log.levels.ERROR)
+  end
+end
 
-  -- Comments
-  italic_commments = true,
-
-  -- Apply colors to terminal
-  terminal_colors = true
-
-
-})
-vim.cmd.colorscheme "vscode"
+SetColorScheme('vscode')
